8bbc9a2ce63352b429627ecda0814c5093db7a44
Yehia Salem <yehiasalem@Yehias-MacBook-Air.local>
2025-11-22 08:31:30 +0200

    Fix routing for GitHub Pages using HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { LanguageProvider } from './LanguageContext.jsx'
 import App from './App.jsx'
 import Portfolio from './Portfolio.jsx'
 import './index.css'
 
-const basename = import.meta.env.BASE_URL
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <LanguageProvider>
-      <BrowserRouter basename={basename}>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/portfolio" element={<Portfolio />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </LanguageProvider>
   </React.StrictMode>,
 )
